8b86f16dcddd7d608a1edee6755da7d54e6fef30
Ramon Vermeulen <ramonvermeulen@hotmail.com>
2022-02-09 15:07:01 +0100

    connect with airbyte config database through psycopg2

--- airbyte-integrations/connectors/source-exact/source_exact/source.py
+++ airbyte-integrations/connectors/source-exact/source_exact/source.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 from typing import Any, Iterable, List, Mapping, Optional, Tuple
 
 import requests
+import psycopg2
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
@@ -9,7 +10,7 @@ from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
 
 
 class SourceExact(AbstractSource):
-    def get_refresh_token(self):
+    def get_refresh_token(self) -> str:
         """
         1. Get a refresh token from a static location
         2. Check if this refresh token is valid by calling an endpoint
@@ -17,14 +18,21 @@ class SourceExact(AbstractSource):
             2.2 If refresh fails check if there is a new token in static location and retry (in case of 2 simultaneous refreshes)
         3. return valid refresh token
 
+        get airbyte-db ip (host) by using:
+        docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' airbyte-db
         """
-        raise NotImplementedError()
+        conn = psycopg2.connect(dbname="airbyte", user="docker", password="docker", host="172.18.0.4", port="5432")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM airbyte_metadata;")
+        result = cur.fetchall()
+        print(result)
+        return "refresh-token"
 
     def get_authenticator(self, config):
         refresh_token = self.get_refresh_token()
 
         return Oauth2Authenticator(
-            token_refresh_endpoint="<ENDPOINT>",
+            token_refresh_endpoint=config["token_refresh_endpoint"],
             client_id=config["client_id"],
             client_secret=config["client_secret"],
             refresh_token=refresh_token,
@@ -69,8 +77,6 @@ class ExactHttpStream(HttpStream):
     ) -> Iterable[Mapping]:
         pass
 
-    ...
-
 
 class IncrementalExactHttpStream(ExactHttpStream):
     _path = None
@@ -85,4 +91,3 @@ class CashflowPayments(IncrementalExactHttpStream):
 
 class CashflowReceivables(IncrementalExactHttpStream):
     _path = "/bulk/Cashflow/Receivables"
-
